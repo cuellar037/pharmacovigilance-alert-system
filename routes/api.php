5bba7d897734d282b4e1348788218648921346bc
Jose Alejandro Cuellar Menza <jcuellarmenza@gmail.com>
2026-08-19 21:24:12 -0500

    fix: correct customer detail route to use CustomerController and plural path

--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,23 @@
 <?php
 
 use App\Http\Controllers\Api\AlertController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CustomerController;
+use App\Http\Controllers\Api\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\OrderController;
-
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function (){
+Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', fn(Request $request) => $request->user());
+    Route::get('/user', fn (Request $request) => $request->user());
 
-    Route::get('/orders', [OrderController::class, 'index']); 
+    Route::get('/orders', [OrderController::class, 'index']);
     Route::get('/orders/export', [OrderController::class, 'export']);
-    Route::get('/orders/{order}', [OrderController::class, 'show']); 
-    Route::get('/customer/{customer}', [OrderController::class, 'show']); 
+    Route::get('/orders/{order}', [OrderController::class, 'show']);
+
+    Route::get('/customers/{customer}', [CustomerController::class, 'show']);
 
     Route::post('/alerts/send', [AlertController::class, 'send']);
-
-
-
 });
